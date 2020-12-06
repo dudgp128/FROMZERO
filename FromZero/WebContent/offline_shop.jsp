cd9b3e49b595709e1aea879001cbb57964f25860
DESKTOP-VC91194\user <nbv19@naver.com>
2020-12-06 16:56:51 +0900

    오늘 제발 끝냈으면 좋겠다 유하,민서,소희 사랑해

--- FromZero/WebContent/offline_shop.jsp
+++ FromZero/WebContent/offline_shop.jsp
@@ -14,12 +14,10 @@
 <meta charset="UTF-8">
 <%
 	ResultSet rs = (ResultSet) request.getAttribute("rs");
-String storename = (String) request.getAttribute("storename");
-int storeid = 0;
+	String storename = (String) request.getAttribute("storename");
+	int offline_storeid = (int) request.getAttribute("offline_storeid");
 String store_img = null;
-if (rs.next()) {
-	storeid = rs.getInt(2);
-	store_img = "images/offline_store-" + storeid + ".jpg";
+	store_img = "images/offline_store-" + offline_storeid + ".jpg";
 %>
 <title>From zero</title>
 </head>
@@ -31,44 +29,47 @@ if (rs.next()) {
 		<h2 class="big-category-text"></h2>
 		<div class="div-shop-grid">
 			<ul class="ul-shop-grid">
-				<img id="img_td" src=<%=store_img%> height="185px"/>
-				<br/>
-				<br/>
-				<form style="margin-left:150px; width:90%" method="post" action="reserve_Offline">
-				<%
-					}
-				String img;
-				String img_li = null;
-				while (rs.next()) {
-				int productid=0;
-				String productname = null;
-				String price = null;
-				productid=rs.getInt(1);
-				productname = rs.getString("offlineproduct_name");
-				price = rs.getString("offlineproduct_price");
-				img = rs.getString("offlineproduct_id");
-				img_li = "offline_store_img/Store" + storeid + "/" + img + ".jpg";
-
-				System.out.println(productname);
-				%>
-				<li id="li-living-item-box">
+				<img id="img_td" src=<%=store_img%> height="185px" />
+				<form style="margin-left: 150px; width: 90%" method="post"
+					action="reserve_Offline">
+					<button style="float:right; margin-right:100px" type="submit">예약하기</button>
+					<br/>
+					<br/>
+					<%
+					String img;
+					String img_li = null;
+					while (rs.next()) {
+					int productid = 0;
+					String productname = null;
+					String price = null;
+					productid = rs.getInt(1);
+					productname = rs.getString("offlineproduct_name");
+					price = rs.getString("offlineproduct_price");
+					img = rs.getString("offlineproduct_id");
+					img_li = "offline_store_img/Store" + offline_storeid + "/" + img + ".jpg";
+					System.out.println(img_li);
+					System.out.println(productname);
+					%>
+					<li id="li-living-item-box">
 						<div class="div-display-living-box">
-							<img class="img-display-box" style="width:src="<%=img_li%>" alt="">
+							<img class="img-display-box" src="
+								<%=img_li%>" alt="">
 							<div class="display-text">
 								<strong><%=productname%></strong>
 								<p><%=price%>원
 								</p>
 								<p>
-								<input id="productNum" type="number" name="<%=productid %>"
-               min="0" step="1" />
-               </p>
+									<input id="productNum" type="number" name="<%=productid%>"
+										min="0" step="1" />
+								</p>
 							</div>
 						</div>
-				</a></li>
-				<%
-					}
-				%><p>
-				<button type="submit">포인트 예약하기</button></p>
+					</li>
+					<%
+						}
+					%><p>
+						
+					</p>
 				</form>
 			</ul>
 		</div>
--- FromZero/src/web/DoOffline.java
+++ FromZero/src/web/DoOffline.java
@@ -20,51 +20,80 @@ import model.DBUtil;
 @WebServlet("/doOffline")
 public class DoOffline extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public DoOffline() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public DoOffline() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		response.setCharacterEncoding("UTF-8");
-		
-		String offline_store=request.getParameter("name_td");
-		
-		ServletContext sc=getServletContext();
-		Connection con = (Connection) sc.getAttribute("DBconnection");
-		
-		String sqlSt="SELECT * FROM offline_product WHERE storename LIKE ";
-		sqlSt=sqlSt+"'%"+offline_store+"%'";
-		ResultSet rs=DBUtil.findStore(con, sqlSt);
-		int storeid=0;
-		
-		if (rs != null) {
-			try {
-				request.setAttribute("rs", rs);
-				request.setAttribute("storename", offline_store);
-				RequestDispatcher view = request.getRequestDispatcher("offline_shop.jsp");
-				view.forward(request, response);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		
-		System.out.println(sqlSt);
 
-	}
-	}
+		String offline_store = request.getParameter("name_td");
+
+		ServletContext sc = getServletContext();
+		Connection con = (Connection) sc.getAttribute("DBconnection");
+
+		String store_sqlSt = "SELECT * FROM offline_store WHERE storename LIKE ";
+		store_sqlSt = store_sqlSt + "'%" + offline_store + "%'";
+		System.out.println(store_sqlSt);
+
+		ResultSet store_rs = DBUtil.findStore(con, store_sqlSt);
+		int i = 0;
+		if (store_rs != null) {
+			try {
+				if (store_rs.next()) {
+					i = store_rs.getInt(1);
+				}}
+			catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+
+			String product_sqlSt = "SELECT * FROM offline_product WHERE storeid=" + i;
+			System.out.println(product_sqlSt);
+			ResultSet rs = DBUtil.findProduct(con, product_sqlSt);
+
+			if (rs != null) {
+				try {
+					request.setAttribute("rs", rs);
+					request.setAttribute("storename", offline_store);
+					request.setAttribute("offline_storeid", i);
+					RequestDispatcher view = request.getRequestDispatcher("offline_shop.jsp");
+					view.forward(request, response);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		/*
+		 * String sqlSt="SELECT * FROM offline_product WHERE storename LIKE ";
+		 * sqlSt=sqlSt+"'%"+offline_store+"%'"; ResultSet rs=DBUtil.findStore(con,
+		 * sqlSt); int storeid=0;
+		 * 
+		 * if (rs != null) { try { request.setAttribute("rs", rs);
+		 * request.setAttribute("storename", offline_store); RequestDispatcher view =
+		 * request.getRequestDispatcher("offline_shop.jsp"); view.forward(request,
+		 * response); } catch (Exception e) { e.printStackTrace(); }
+		 * 
+		 * System.out.println(sqlSt);
+		 */
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
