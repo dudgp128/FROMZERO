cdab8509f763e05dcb04d28e41607b2d1f5057aa
sowhitt <0514leesohee@gmail.com>
2020-12-12 20:15:44 +0900

    마이페이지-누적 구매금액

---
+++ FromZero/src/model/NameAscending.java
@@ -0,0 +1,17 @@
+package model;
+
+import java.util.Comparator;
+
+/*Test객체의 name을 기준으로 오름차순 정렬하기*/
+public class NameAscending implements Comparator<Product> {
+ 
+    @Override
+    public int compare(Product a, Product b) {
+        
+        String temp1 = a.getProductname();
+        String temp2 = b.getProductname();
+        
+        return temp1.compareTo(temp2);
+        /*return a.getName().compareTo(b.getName());*/
+    }
+}
