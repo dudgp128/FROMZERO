f7fd208f54c48de3b7fae036d6f235d4d61cfe4a
msyou999 <msyou999@gmail.com>
2020-12-13 15:42:56 +0900

    1-35 이미지 파일 & 로그인 시 000님 환영합니다 바로 뜨게 수정 & html 파일 다시 삭제

--- FromZero/src/web/DoLogin.java
+++ FromZero/src/web/DoLogin.java
@@ -19,68 +19,66 @@ import javax.swing.JOptionPane;
 import model.DBUtil;
 import model.User;
 
-
 /**
  * Servlet implementation class DoLogin
  */
 @WebServlet("/doLogin")
 public class DoLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public DoLogin() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("UTF-8"); 	//한글깨짐 방지
+	public DoLogin() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8"); // 한글깨짐 방지
 		response.setCharacterEncoding("UTF-8");
 		String mid = request.getParameter("mid");
 		String passwd = request.getParameter("passwd");
 		String name = null;
 		String address = null;
 		String phone = null;
-		
+
 		ServletContext sc = getServletContext();
-		Connection conn= (Connection) sc.getAttribute("DBconnection");
+		Connection conn = (Connection) sc.getAttribute("DBconnection");
 
 		ResultSet rs = DBUtil.findUser(conn, mid);
 
 		PrintWriter out = response.getWriter();
 		if (rs != null) {
-			try{
-				if(rs.next()) { // existing user
-						String checkpw = rs.getString("passwd");
-						name = rs.getString("name");
-						address = rs.getString("address");
-						phone = rs.getString("phone");
-						
-						
-						User user = new User(mid, passwd, name, address, phone);
+			try {
+				if (rs.next()) { // existing user
+					String checkpw = rs.getString("passwd");
+					name = rs.getString("name");
+					address = rs.getString("address");
+					phone = rs.getString("phone");
 
-						if(checkpw.equals(passwd)){// valid user and passwd
-							request.setAttribute("user", user);
-							RequestDispatcher view = request.getRequestDispatcher("main.jsp");
-						    view.forward(request, response);
-						    
-						    HttpSession session=request.getSession(); //세션 반환 (세선 ㅇ-반환, 세션x-생성)
-							session.setAttribute("user_id", mid); //세션 값 넣기
-							session.setAttribute("user_name", name);
-						}
-						else{// wrong passwd
-							response.setContentType("text/html; charset=UTF-8");
-							PrintWriter pout = response.getWriter();
-							pout.println("<script>alert('잘못된 비밀번호입니다.'); location.href='login.html';</script>");
-							pout.flush();
-						}
-				}
-				else { // invalid user
+					User user = new User(mid, passwd, name, address, phone);
+
+					if (checkpw.equals(passwd)) {// valid user and passwd
+						request.setAttribute("user", user);
+
+						HttpSession session = request.getSession(); // 세션 반환 (세선 ㅇ-반환, 세션x-생성)
+						session.setAttribute("user_id", mid); // 세션 값 넣기
+						session.setAttribute("user_name", name);
+						RequestDispatcher view = request.getRequestDispatcher("main.jsp");
+						view.forward(request, response);
+					} else {// wrong passwd
+						response.setContentType("text/html; charset=UTF-8");
+						PrintWriter pout = response.getWriter();
+						pout.println("<script>alert('잘못된 비밀번호입니다.'); location.href='login.html';</script>");
+						pout.flush();
+					}
+				} else { // invalid user
 					response.setContentType("text/html; charset=UTF-8");
 					PrintWriter pout = response.getWriter();
 					pout.println("<script>alert('가입되지 않은 아이디입니다.'); location.href='login.html';</script>");
@@ -88,17 +86,18 @@ public class DoLogin extends HttpServlet {
 				}
 			} catch (SQLException e) {
 				e.printStackTrace();
-			} //try end
+			} // try end
 		} // if end
-		} // doGet() end
-	
+	} // doGet() end
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		request.setCharacterEncoding("UTF-8"); 	//한글깨짐 방지
+		request.setCharacterEncoding("UTF-8"); // 한글깨짐 방지
 		response.setCharacterEncoding("UTF-8");
 		doGet(request, response);
 	}
